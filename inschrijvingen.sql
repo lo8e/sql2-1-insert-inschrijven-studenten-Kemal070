7f1935f114d51f5eb263d215abe3e301792847d1
Kemal070 <47315234+Kemal070@users.noreply.github.com>
2019-05-17 16:22:17 +0200

    opdracht 2

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,4 +1,7 @@
 USE lo8e_sql2;
 
-INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code)
-VALUES ('lo9e-amo1', 'vlt03', '2019', 'amo'),('lo9e-amo2', 'rs001', '2019', 'amo'),('lo9e-amo3', 'bkr02', '2019', 'amo');
+INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voorletters, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, actief, inschrijvings_datum)
+VALUES ('90506', 'habibti', 'van der', 'M', 'Michael', '2001-05-25', '1234AB', 'Den Haag', 75.5, 1.65, 1, '2015-01-02'),
+('90502', 'qwdqwwq', 'van', 'M', 'Michael', '2001-05-25', '2983AQ', 'Den Haag', 71.5, 1.25, 1, '2015-01-02'),
+('90503', 'asdddq', 'van', 'M', 'Michael', '2001-05-25', '1237CB', 'Den Haag', 78.5, 1.55, 1, '2015-01-02'),
+('90504', 'zzzzzzz', 'van der', 'Q', 'Quinten', '2021-05-25', '1234BA', 'Den Haag', 72.5, 2.65, 1, '2015-01-02');
