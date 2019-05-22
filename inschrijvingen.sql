2ea0df4247802ad64dd13764d8779ef15f07e67a
Kemal070 <47315234+Kemal070@users.noreply.github.com>
2019-05-22 21:40:48 +0200

    Opdracht 4

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,4 +1,5 @@
 USE lo8e_sql2;
 
-INSERT INTO klassen_studenten (klas_code, ov_nummer)
-VALUES ('LO9E-AMO1', '90502'),('LO9E-AMO2', '90503'),('LO9E-AMO3', '90504'),('LO9E-AMO3', '90506');
+UPDATE studenten
+SET geboortedatum = '2012-12-12'
+WHERE ov_nummer = '90506';
