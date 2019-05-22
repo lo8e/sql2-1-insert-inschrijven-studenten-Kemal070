88517a66d7504c0d173a8ab2e2110e797f33e13d
Kemal070 <47315234+Kemal070@users.noreply.github.com>
2019-05-22 21:37:54 +0200

    Opgave 3

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,7 +1,4 @@
 USE lo8e_sql2;
 
-INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voorletters, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, actief, inschrijvings_datum)
-VALUES ('90506', 'habibti', 'van der', 'M', 'Michael', '2001-05-25', '1234AB', 'Den Haag', 75.5, 1.65, 1, '2015-01-02'),
-('90502', 'qwdqwwq', 'van', 'M', 'Michael', '2001-05-25', '2983AQ', 'Den Haag', 71.5, 1.25, 1, '2015-01-02'),
-('90503', 'asdddq', 'van', 'M', 'Michael', '2001-05-25', '1237CB', 'Den Haag', 78.5, 1.55, 1, '2015-01-02'),
-('90504', 'zzzzzzz', 'van der', 'Q', 'Quinten', '2021-05-25', '1234BA', 'Den Haag', 72.5, 2.65, 1, '2015-01-02');
+INSERT INTO klassen_studenten (klas_code, ov_nummer)
+VALUES ('LO9E-AMO1', '90502'),('LO9E-AMO2', '90503'),('LO9E-AMO3', '90504'),('LO9E-AMO3', '90506');
